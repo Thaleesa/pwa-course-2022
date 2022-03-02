494e9fb81ff385c21373e052691b99f8cfdfa6bc
Thaleesa <thaleesa.khum@bumail.net>
2022-03-02 22:43:44 +0700

    1630703179

--- src/Steamchat.js
+++ src/Steamchat.js
@@ -2,12 +2,13 @@ import './css/Steamchat.css';
 function Steamchat(){
     return (
         <div>
-        <div className='Steamchat'>Hola! Steamchat is READY! :3
+        <div className='Steamchat'>Hi! Welcome to Steamchat!</div>
         <div className='ball1'> おこにちわ :D </div>
 
         </div>
-    </div>
+    
     )
-}
-
+    
+    }
 export default Steamchat;
+    
